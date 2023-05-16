2c553dc5b847675514767bcd0b1621dfc515d395
Sai Vara Prasad Degala <93783719+Saivaraprasad48@users.noreply.github.com>
2023-05-16 21:33:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import {Route, Switch} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 
 import LoginForm from './components/LoginForm'
 import Home from './components/Home'
+import Products from './components/Products'
+import Cart from './components/Cart'
 import NotFound from './components/NotFound'
 
 import './App.css'
@@ -10,8 +12,10 @@ const App = () => (
   <Switch>
     <Route exact path="/login" component={LoginForm} />
     <Route exact path="/" component={Home} />
+    <Route exact path="/products" component={Products} />
+    <Route exact path="/cart" component={Cart} />
     <Route component={NotFound} />
   </Switch>
 )
 
-export default App
+export default App 
